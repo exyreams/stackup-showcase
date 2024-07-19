cf51ab391f37364f143410ac3c11251c4c4a6269
exyreams <exyream@gmail.com>
2024-07-19 23:53:52 +0545

    add: card

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 import Tooltips from "./Tootips";
+import { Card } from "./Card";
 
-export { Navbar, Footer, Sidebar, Tooltips };
+export { Navbar, Footer, Sidebar, Tooltips, Card };
