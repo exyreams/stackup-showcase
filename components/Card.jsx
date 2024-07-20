4482725d1ed0f7b1f70a262b7868934fb41a1edf
exyreams <exyream@gmail.com>
2024-07-20 15:51:13 +0545

    add:view

--- components/Card.jsx
+++ components/Card.jsx
@@ -7,7 +7,7 @@ import { user } from "../assets/";
 
 export function Card({ description, projectname, techstack, username }) {
   return (
-    <div className="w-350 group/card">
+    <div className='w-350 group/card'>
       <div className=" cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto backgroundImage flex flex-col justify-between p-4">
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
         <div className="flex flex-row items-center space-x-4 z-10">
